e77b9dde54427c091bb0452f88276fa39bcb4147
Radi <radi.rusadi.s@gmail.com>
2019-02-25 12:16:11 +0700

    Adding helper asynStorage

--- src/screens/splashScreen.js
+++ src/screens/splashScreen.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar, View, Text } from 'react-native';
 
 class SplashScreen extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
         <Text> Splash Screen </Text>
       </View>
     );
   }
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  }
+};
 
 export default SplashScreen;
